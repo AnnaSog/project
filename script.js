651b7ab0c1c1da33f98abcb1420c1d7a439b4a8b
Vartanush <anna.sob0711@gmail.com>
2021-01-03 14:22:54 +0300

    'second'

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 "use strict";
 
-let incr = 10,
-    decr = 10;
 
-console.log(++incr);
-console.log(--decr);
+const numberOfFilm = prompt ("Сколько фильмов вы уже посмотрели?", "");
+
+
+const personalMovieDB = {
+ count: numberOfFilm, 
+ movies: {},
+ actors: {},
+ genres: [],
+ privat: false
+
+};
+
+const a = prompt ('Один из последних фильмов?','' ),
+      b=prompt ('На сколько оцените его?','' ),
+      c = prompt ('Один из последних фильмов?', ''),
+      d=prompt ('На сколько оцените его?','');
+ 
+
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+
+console.log(personalMovieDB);
