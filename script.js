e68855e499f841eab95851c53666de787131a3fe
Vartanush <anna.sob0711@gmail.com>
2021-01-04 19:26:46 +0300

    third

--- script.js
+++ script.js
@@ -13,54 +13,31 @@ const personalMovieDB = {
 };
 
 
-// if (personalMovieDB.count <10){
-//     console.log("Просмотрено довольно мало фильмов");
-//    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <30) {
-//      console.log('Вы классический зритель');
-//    } else if (personalMovieDB.count >=30){
-//     console.log('Вы киноман!');
-//    }else {
-//        console.log ("Произошла ошибка!");
+if (personalMovieDB.count <10){
+    console.log("Просмотрено довольно мало фильмов");
+   } else if (personalMovieDB.count >= 10 && personalMovieDB.count <30) {
+     console.log('Вы классический зритель');
+   } else if (personalMovieDB.count >=30){
+    console.log('Вы киноман!');
+   }else {
+       console.log ("Произошла ошибка!");
    
-// }
+}
 
 
-// for (let i =0; i < 2; i++){
-//     const a = prompt ('Один из последних фильмов?','' ),
-//           b=prompt ('На сколько оцените его?','' );
+for (let i =0; i < 2; i++){
+    const a = prompt ('Один из последних фильмов?','' ),
+          b=prompt ('На сколько оцените его?','' );
     
-//     if (a !=null && b !=null && a !="" && b!="" && a.length <50){
-//       personalMovieDB.movies[a]=b; 
-//        console.log("done");
-//     } else {
-//         console.log ('error');
-//         i--;
-//     } 
-// }
+    if (a !=null && b !=null && a !="" && b!="" && a.length <50){
+      personalMovieDB.movies[a]=b; 
+       console.log("done");
+    } else {
+        console.log ('error');
+        i--;
+    } 
+}
 
-// let i = 0;
-// while (i < 2) {
-//     const a = prompt ('Один из последних фильмов?','' ),
-//           b=prompt ('На сколько оцените его?','' ); 
 
-//     // if (a !=null && b !=null && a !="" && b!="" && a.length <50){
-//         personalMovieDB.movies[a]=b; 
-//         // console.log("done");
-//     // } else {
-//     //     console.log ('error');
-//     //     i--;
-//     // } 
-//     console.log(i);
-//     i++;
-// }
 
-let i = 0;
-do {
-   const a = prompt ('Один из последних фильмов?','' ),
-         b=prompt ('На сколько оцените его?','' );
-    personalMovieDB.movies[a]=b; 
-
-  console.log ("done");
-    i++;
-} while (i<2); 
 
